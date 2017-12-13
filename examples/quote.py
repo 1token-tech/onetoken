84af6f1749f7f9b23fc09f130b1d00b0cbf24fb6
Dawei <markx914@gmail.com>
2017-12-13 17:21:52 +0800

    add example

--- examples/quote.py
+++ examples/quote.py
@@ -7,7 +7,7 @@ def on_update(tk):
     print(tk)
 
 
-async def main():
+async def sub_func():
     contract = 'btc.usd:xtc.bitfinex'
     quote = Quote()
     await quote.init()
@@ -17,6 +17,22 @@ async def main():
         await asyncio.sleep(2)
         # tk = quote.get_last_tick(contract)
         # print(tk)
+
+
+async def get_last():
+    contract = 'btc.usd:xtc.bitfinex'
+    quote = Quote()
+    await quote.init()
+    await quote.subscribe_tick(contract)
+
+    while True:
+        await asyncio.sleep(2)
+        tk = quote.get_last_tick(contract)
+        print(tk)
+
+
+async def main():
+    await sub_func()
 
 
 if __name__ == '__main__':
