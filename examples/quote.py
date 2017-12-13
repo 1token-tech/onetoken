87495481ff44c29b1d136e4899dfbea04c0e5451
Dawei <markx914@gmail.com>
2017-12-13 17:19:28 +0800

    add call back function for subscribing tick

--- examples/quote.py
+++ examples/quote.py
@@ -3,16 +3,20 @@ import asyncio
 from btp import Quote
 
 
+def on_update(tk):
+    print(tk)
+
+
 async def main():
     contract = 'btc.usd:xtc.bitfinex'
     quote = Quote()
     await quote.init()
-    await quote.subscribe_tick(contract)
+    await quote.subscribe_tick(contract, on_update)
 
     while True:
         await asyncio.sleep(2)
-        tk = quote.get_last_tick(contract)
-        print(tk)
+        # tk = quote.get_last_tick(contract)
+        # print(tk)
 
 
 if __name__ == '__main__':
