a7ee08b41f4d7d3fbcb4f3fd002e6e1a58edba9c
Dawei <markx914@gmail.com>
2017-12-14 14:36:23 +0800

    update logger

--- btp/quote.py
+++ btp/quote.py
@@ -3,6 +3,7 @@ import aiohttp
 import json
 
 from btp import Ticker
+from .logger import log
 
 
 class Quote:
@@ -18,14 +19,15 @@ class Quote:
             self.sess.close()
 
     async def init(self):
-        print(f'Connecting to {self.host}')
+        log.debug(f'Connecting to {self.host}')
+
         try:
             self.sess = aiohttp.ClientSession()
             self.ws = await self.sess.ws_connect(self.host, autoping=False)
         except Exception as e:
             self.error(e, 'try connect to WebSocket failed...')
         else:
-            print('Connected to WS')
+            log.debug('Connected to WS')
 
             asyncio.ensure_future(self.on_msg())
 
@@ -37,14 +39,13 @@ class Quote:
                     # print(msg.data)
                     self.parse_tick(msg.data)
                 elif msg.type == aiohttp.WSMsgType.CLOSED:
-                    print('closed')
+                    log.debug('closed')
                     break
                 elif msg.type == aiohttp.WSMsgType.ERROR:
-                    print('error', msg)
+                    log.warning('error', msg)
                     break
             except Exception as e:
-                print('ws was disconnected...')
-                print(e)
+                log.warning('ws was disconnected...', e)
 
     # await ws.send_json({'uri': 'subscribe-single-tick-verbose', 'contract': 'btc.usd:xtc.bitfinex'})
 
@@ -58,20 +59,20 @@ class Quote:
                 if tick.contract in self.tick_queue:
                     self.tick_queue[tick.contract].put_nowait(tick)
         except:
-            print('parse error')
+            log.warning('parse error')
 
     async def subscribe_tick(self, contract, on_update=None):
         if self.ws:
             try:
                 await self.ws.send_json({'uri': 'subscribe-single-tick-verbose', 'contract': contract})
             except Exception as e:
-                print(f'subscribe {contract} failed...', e)
+                log.warning(f'subscribe {contract} failed...', e)
             else:
                 self.tick_queue[contract] = asyncio.Queue()
                 if on_update:
                     asyncio.ensure_future(self.handle_q(contract, on_update))
         else:
-            print('ws is not ready...')
+            log.warning('ws is not ready yet...')
 
     async def handle_q(self, contract, on_update):
         while contract in self.tick_queue:
