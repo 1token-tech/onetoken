a06015a03ff9aee77445e292a174480ba725eb91
Dawei <markx914@gmail.com>
2017-12-18 14:50:29 +0800

    change default exchange to mock

--- examples/account.py
+++ examples/account.py
@@ -26,7 +26,7 @@ def check_auth_file():
 async def main():
     check_auth_file()
 
-    acc = Account('leo@xtc.huobip')
+    acc = Account('leo@xtc.mock')
 
     # 获取账号 info
     info, err = await acc.get_info()
