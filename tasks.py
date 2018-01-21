ed045eb8ab709f18ef42a34edc44f84a56c89922
Youze Tang <tangyouze@gmail.com>
2018-01-21 13:30:41 +0800

    update inv script

--- tasks.py
+++ tasks.py
@@ -4,9 +4,9 @@ import invoke
 @invoke.task
 def upload(ctx):
     ctx.run('python setup.py sdist bdist_wheel upload')
-    ctx.run('rm -rf build dist qbx.egg-info')
+    ctx.run('rm -rf build dist ots.egg-info')
 
 
 @invoke.task
 def clean(ctx):
-    ctx.run('rm -rf build dist qbx.egg-info')
+    ctx.run('rm -rf build dist ots.egg-info')
