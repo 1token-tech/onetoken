0f593aefebf42cf174f37e233d9489c79ee054f6
Dawei <markx914@gmail.com>
2018-02-03 13:53:38 +0800

    change package name to onetoken

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='onetoken-trade',
+setup(name='onetoken',
       author='QbTrade',
       url='https://github.com/qbtrade/ots',
       author_email='markx914@gmail.com',
