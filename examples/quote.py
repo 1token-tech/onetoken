d42cce63b4f3ba4a6c796b05d0e34e3fe62ad682
Youze Tang <tangyouze@gmail.com>
2018-03-22 16:49:37 +0800

    update quote

--- examples/quote.py
+++ examples/quote.py
@@ -4,23 +4,23 @@ import onetoken as ot
 from onetoken import Tick
 
 
-def on_update(tk: Tick):
-    print('tick come', tk)
+def on_update_1(tk: Tick):
+    print('tick come 1', tk)
 
 
-def on_update2(tk: Tick):
+def on_update_2(tk: Tick):
     print('tick come 2', tk)
 
 
-async def sub_func():
-    contract = 'okex/ltc.btc'
-    await ot.quote.subscribe_tick(contract, on_update)
+async def subscribe_from_ws():
+    contract = 'okex/btc.usdt'
+    await ot.quote.subscribe_tick(contract, on_update_1)
 
-    contract = 'okex/ltc.btc'
-    await ot.quote.subscribe_tick(contract, on_update2)
+    contract = 'okex/btc.usdt'
+    await ot.quote.subscribe_tick(contract, on_update_2)
 
-    contract = 'okex/eth.btc'
-    await ot.quote.subscribe_tick(contract, on_update2)
+    contract = 'binance/eth.btc'
+    await ot.quote.subscribe_tick(contract, on_update_2)
 
 
 async def get_last():
@@ -29,12 +29,14 @@ async def get_last():
     while True:
         await asyncio.sleep(2)
         tk, err = await ot.quote.get_last_tick(contract)
-        print(tk, err)
+        print('tick get by last', tk, err)
 
 
 async def main():
-    await sub_func()
+    await subscribe_from_ws()
     await get_last()
+    while True:
+        await asyncio.sleep(1)
 
 
 if __name__ == '__main__':
