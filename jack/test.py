1ea70e93b3e6e017e6d8cd6461afe18d78c6e7ad
ZTjack <jack.jiang.scivan@gmail.com>
2020-09-18 18:19:28 +0800

    add info ws  tick ws together

--- jack/test.py
+++ jack/test.py
@@ -1,23 +1,192 @@
 """
-Usage:
-    tick.py [options]
-
-Options:
-
+Author: Jack
+Date: 2020-09-18 14:29:34
+LastEditors: Jack
+LastEditTime: 2020-09-18 14:34:20
+Description: 
 """
+import arrow
 import _thread as thread
 import json
 import logging
 from collections import defaultdict
 
 import arrow
+import hashlib
+import gzip
 import queue
+import hmac
 import time
 import websocket
 from websocket import ABNF
 
 
-# 为一个Websocket多包一层
+class AccountWs:
+    def __init__(self,
+                 symbol: str = None,
+                 api_key: str = None,
+                 api_secret: str = None):
+        """
+
+        :param symbol: exchange/account
+        :param api_key: str
+        :param api_secret: str
+        """
+        self.ws = None
+        self.pong = 0
+        self.symbol = symbol
+        self.exchange, self.account = symbol.split('/', 1)
+        self.host_ws = 'wss://cdn.1tokentrade.cn/api/v1/ws/trade/{}/{}'.format(
+            self.exchange, self.account)
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.sub_key = set()
+        self.handle_info = None
+        self.handle_order = None
+
+    @staticmethod
+    def gen_sign(secret, verb, path, nonce, data_str):
+        """
+        签名方法
+        :param secret:
+        :param verb:
+        :param path:
+        :param nonce:
+        :param data_str:
+        :return:
+        """
+        message = verb + path + str(nonce) + data_str
+        print('sign message', message)
+        signature = hmac.new(bytes(secret, 'utf8'),
+                             bytes(message, 'utf8'),
+                             digestmod=hashlib.sha256).hexdigest()
+        return signature
+
+    def ws_connect(self):
+        print('Connecting to {}'.format(self.host_ws))
+        nonce = str(int(time.time() * 1000000))
+        sign = self.gen_sign(self.api_secret,
+                             'GET',
+                             path='/ws/' + self.account,
+                             nonce=nonce,
+                             data_str='')
+        headers = {
+            'Api-Nonce': str(nonce),
+            'Api-Key': self.api_key,
+            'Api-Signature': sign
+        }
+        self.ws = websocket.WebSocketApp(self.host_ws,
+                                         header=headers,
+                                         on_open=self.on_open,
+                                         on_data=self.on_data,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close)
+        self.ws.run_forever()
+
+    def send_message(self, message):
+        """
+        通过 websocket 发送消息
+        :param message: string
+        :return: None
+        """
+        self.ws.send(message)
+
+    def send_json(self, js):
+        """
+        通过 websocket 发送json
+        :param js: dict
+        :return: None
+        """
+        self.send_message(json.dumps(js))
+
+    def heart_beat_loop(self):
+        while self.ws and self.ws.keep_running:
+            try:
+                if time.time() - self.pong > 25:
+                    logging.warning('connection heart beat lost')
+                    break
+                else:
+                    ping = time.time()
+                    self.send_json({'uri': 'ping', 'uuid': ping})
+            except:
+                logging.exception('unexpected failed')
+            time.sleep(10)
+
+    def on_data(self, msg, msg_type, *args):
+        try:
+            if msg_type == ABNF.OPCODE_BINARY or msg_type == ABNF.OPCODE_TEXT:
+                if msg_type == ABNF.OPCODE_TEXT:
+                    data = json.loads(msg)
+                else:
+                    data = json.loads(gzip.decompress(msg).decode())
+                uri = data.get('uri')
+                if uri == 'pong':
+                    self.pong = time.time()
+                elif uri in ['connection', 'status']:
+                    if data.get('code', data.get('status',
+                                                 None)) in ['ok', 'connected']:
+                        for key in self.sub_key:
+                            self.send_json({'uri': 'sub-{}'.format(key)})
+                elif uri == 'info':
+                    if data.get('status', 'ok') == 'ok':
+                        info = data['data']
+                        self.handle_info(info)
+                elif uri == 'order':
+                    if data.get('status', 'ok') == 'ok':
+                        for order in data['data']:
+                            self.handle_order(order)
+                elif uri in ['sub-order', 'sub-info']:
+                    print(data['uri'], data['code'])
+                else:
+                    print('unhandled', data)
+        except:
+            logging.exception('unexpected failed')
+
+    def on_open(self):
+        print('connected on open')
+        self.pong = time.time()
+        threading.Thread(target=self.heart_beat_loop).start()
+
+    @staticmethod
+    def on_error(ws, error):
+        """
+        websocket 发生错误的回调
+        :param error:
+        :return: None
+        """
+        print('on error', error)
+        logging.exception(error)
+
+    @staticmethod
+    def on_close(*args):
+        """
+        websocket 关闭的回调
+        :return: None
+        """
+        print("### websocket closed ###")
+
+    def run(self) -> None:
+        """
+        运行 websocket
+        :return: None
+        """
+        self.ws_connect()
+
+    def sub_info(self, callback=None):
+        def _handle_info(data):
+            print('get new info', data)
+
+        self.sub_key.add('info')
+        self.handle_info = callback if callback else _handle_info
+
+    def sub_order(self, callback=None):
+        def _handle_order(data):
+            print('get new order', data)
+
+        self.sub_key.add('order')
+        self.handle_order = callback if callback else _handle_order
+
+
 class Quote:
     def __init__(self, key, ws_url, data_parser):
         # name
@@ -95,11 +264,12 @@ class Quote:
                     print('subscribe-single-candle', data)
                 else:
                     q_key, parsed_data = self.data_parser(data)
-                    if q_key is None:
+                    contract = json.loads(q_key)['contract']
+                    if contract is None:
                         print('unknown message', data)
                         return
-                    if q_key in self.data_queue:
-                        self.data_queue[q_key].put(parsed_data)
+                    if contract in self.data_queue:
+                        self.data_queue[contract].put(parsed_data)
         except Exception as e:
             print('msg error...', e)
 
@@ -150,7 +320,7 @@ class Quote:
             time.sleep(1)
         sub_data = {'uri': uri}
         sub_data.update(kwargs)
-        q_key = json.dumps(sub_data, sort_keys=True)
+        q_key = json.loads(json.dumps(sub_data, sort_keys=True))['contract']
         with self.lock:
             try:
                 self.send_json(sub_data)
@@ -218,7 +388,7 @@ class TickV3Quote(Quote):
         super().__init__(
             'tick.v3', 'wss://cdn.1tokentrade.cn/api/v1/ws/tick-v3?gzip=true',
             self.parse_tick)
-        self.channel = 'subscribe-single-tick-verbose'
+        self.channel = 'subscribe-single-tick-verbose'  # 'subscribe-single-tick-verbose','subscribe-single-zhubi-verbose','subscribe-single-candle'
         self.ticks = {}
 
     def parse_tick(self, data):
@@ -375,10 +545,6 @@ class Tick:
         return str(self)
 
 
-class Config:
-    print_only_delay = False
-
-
 def on_update(tk: Tick):
     delay = (arrow.now() - tk.time).total_seconds()
     # print(tk)
@@ -389,20 +555,66 @@ def on_update(tk: Tick):
         print('tick delay comes', arrow.now(), 'tick come 1', delay, tk)
 
 
-def get_tick():
-    """
-    订阅一个交易对
-    :return:
-    """
-    tick_v3 = TickV3Quote()
-    tick_v3.run()
-    sub_list = ['binance/btc.usdt']
-    for contract in sub_list:
-        tick_v3.subscribe_tick_v3(contract, on_update)
+class Strategy:
+    def __init__(self, contracts, name: str = None, acc_symbol: str = None):
+        # config related
+        self.name = name  # 策略名
+        self.acc_symbol = acc_symbol  # 账户名 okef/jack
+        self.contracts = contracts  # 标的
+        self.ticks = {}
+        self.inited = False  # 初始化完毕标志
+        self.active_orders = []  # pending orders
+        self.position = {}  # 持仓信息
+        self.info = {}
 
-    time.sleep(20)
-    tick_v3.close()
+    def on_tick_update(tk: Tick):
+        delay = (arrow.now() - tk.time).total_seconds()
+        # print(tk)
+        if tk.bid1 and tk.ask1:
+            if tk.bid1 >= tk.ask1:
+                print('bid1 >= ask1 %s %s', tk.bid1, tk.ask1)
+        if delay > 10:
+            print('tick delay comes', arrow.now(), 'tick come 1', delay, tk)
+
+    def on_info_update(self, data):
+        self.info = data
+        # print('get new info', self.info)
+
+    def on_order_update(self, data):
+        self.active_orders.append(data)
+        print('get new order', self.active_orders)
+
+    def sub_account(self):
+        self.account = AccountWs(
+            symbol=self.acc_symbol,
+            api_key='QCsKNH71-n8AqFBer-ZUdnRnKA-y1jsbYhU',
+            api_secret='s53cGW3W-sdEXvOSA-i8JX9oAz-16mMA4sb')
+        self.account.sub_info(self.on_info_update)
+        self.account.sub_order(self.on_order_update)
+        self.account.run()
+
+    def init(self):
+        self.sub_account()
+
+        self.ticks = TickV3Quote()
+        self.ticks.run()
+        sub_list = self.contracts
+        for contract in sub_list:
+            self.ticks.subscribe_tick_v3(contract, on_tick_update)
+        time.sleep(20)
+        self.ticks.close()
+
+    def printSth(self):
+        # get contract1 tick data
+        print(self.ticks.data_queue[self.contracts[0]].get().ask1)
+
+
+def main():
+    s = Strategy(name='test_strategy',
+                 acc_symbol='okex/mock-jack',
+                 contracts=['okex/btc.usdt', 'okex/eth.usdt'])
+    s.init()
 
 
 if __name__ == '__main__':
-    get_tick()
+    main()
